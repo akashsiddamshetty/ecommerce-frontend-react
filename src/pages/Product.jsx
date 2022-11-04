72675b5ed2f06265e0feee149d9c0ab36cd78040
ktk <akashsiddamshetty@gmail.com>
2022-11-04 22:02:34 +0530

    -- new pages added

---
+++ src/pages/Product.jsx
@@ -0,0 +1,156 @@
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`;
+const ImageContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px; ;
+`;
+const Title = styled.h1`
+  font-weight: 200;
+`;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  width: 50%;
+  margin: 30px 0px;
+  justify-content: space-between;
+`;
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+const FilterSize = styled.select`
+  margin-left: 5px;
+  padding: 5px;
+  cursor: pointer;
+`;
+const FilterSizeOption = styled.option`
+cursor: pointer;
+`;
+const AddContainer = styled.div`
+  display: flex;
+  width: 50%;
+  align-items: center;
+  justify-content: space-between;
+`;
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-weight: 700;
+  cursor: pointer;
+`;
+const Amount = styled.span`
+  margin: 0px 5px;
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: default;
+`;
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover {
+    background-color: teal;
+    color: white;
+  }
+`;
+
+const Product = () => {
+  return (
+    <>
+      <Announcement />
+      <Navbar />
+      <Wrapper>
+        <ImageContainer>
+          <Image src="https://img.freepik.com/free-photo/jeans_1203-8093.jpg?w=2000" />
+        </ImageContainer>
+        <InfoContainer>
+          <Title>Denim Jeans</Title>
+          <Desc>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+            dolorum voluptatibus exercitationem saepe cum fuga non ipsam animi
+            deserunt, illum odio laudantium delectus incidunt perferendis iste
+            veritatis quam a facere?
+          </Desc>
+          <Price>$20</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <RemoveIcon />
+              <Amount>1</Amount>
+              <AddIcon />
+            </AmountContainer>
+            <Button>Add To Cart</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </>
+  );
+};
+
+export default Product;
