53934e9ac21e7fa9eb4f0c2a09f4f1eadac96a73
ktk <akashsiddamshetty@gmail.com>
2022-11-11 12:55:37 +0530

    -- added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <Cart />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="/productlist" element={<ProductList />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  );
 };
 
 export default App;
